c697247b0c7f547dd3a24d4bde61ba6e206e0845
Volodymyr Shevchuk <Volodymyr.Shevchuk@eleks.com>
2018-04-05 16:15:12 +0300

    Added shared package;

---
+++ packages/shared/components/highcharts/highcharts.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HighchartsComponent } from './highcharts.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    HighchartsComponent
+  ]
+})
+export class HighchartsModule { }
