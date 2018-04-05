9a484cd630c90a0dee4cc19377af6242ce35d3f5
Volodymyr Shevchuk <Volodymyr.Shevchuk@eleks.com>
2018-04-05 14:51:20 +0300

    Updated basic structure;

--- packages/splash-page/src/app/app.module.ts
+++ packages/splash-page/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppsListComponent } from './apps-list/apps-list.component';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, AppsListComponent, SplashScreenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: '',
+          component: SplashScreenComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: 'apps-list',
+          component: AppsListComponent
+        },
+        {
+          path: 'login',
+          loadChildren: './login/login.module#LoginModule'
+        }
+      ],
+      { initialNavigation: 'enabled' }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ packages/splash-page/src/app/login/login.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([
+      { path: '', pathMatch: 'full', component: LoginComponent }
+    ])
+  ],
+  declarations: [LoginComponent]
+})
+export class LoginModule {}
