511a0beb732f2be6f13f1df8081ea39e51754cd0
Volodymyr Shevchuk <dosandkv@gmail.com>
2018-04-20 17:16:11 +0300

    Removed old "shaerd" dir;

--- shared/src/app/modules/ui-kit/eagle-control/eagle-control.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-eagle-control',
-  templateUrl: './eagle-control.component.html',
-  styleUrls: ['./eagle-control.component.css']
-})
-export class EagleControlComponent {}
--- shared/src/app/modules/ui-kit/login-shared/index.ts
+++
@@ -1,3 +0,0 @@
-export { LoginSharedModule } from './login-shared.module';
-export { LoginSharedRoutingModule } from './login-shared-routing.module';
-export { LoginSharedComponent } from './login-shared.component';
