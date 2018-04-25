10fa971f2f9df917cc4a1e4940c60af8b138e130
Serhii Krechko <skrechko@eagleinvsys.com>
2018-04-25 20:51:01 +0300

    Move package json to dist folder

--- packages/eagle-lib-ui/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const sharedLib: () => void;
-declare const sharedLib2: () => void;
-declare const sharedLib3: () => void;
-export { sharedLib, sharedLib2, sharedLib3 };
--- packages/eagle-lib-ui/src/lib/index.ts
+++ packages/eagle-lib-ui/src/lib/index.ts
@@ -1,4 +1,5 @@
 const sharedLib = () => console.error('eagle-uikit-lib-32323-777');
 const sharedLib2 = () => console.error('eagle-uikit-lib-XXXXXXXXX');
 const sharedLib3 = () => console.error('eagle-uikit-lib-ZZZZZZZZZZ---xcxc9999');
+// Some comment
 export {sharedLib, sharedLib2, sharedLib3};
