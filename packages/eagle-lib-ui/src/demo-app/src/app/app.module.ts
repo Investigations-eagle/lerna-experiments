ba61c03538951445962ccde477d1438110604657
Serhii Krechko <skrechko@eagleinvsys.com>
2018-04-26 14:37:33 +0300

    Attach lib to playgorund

--- packages/eagle-lib-ui/src/demo-app/src/app/app.module.ts
+++ packages/eagle-lib-ui/src/demo-app/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { EagleControlModule } from 'eagle-lib-ui';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EagleControlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
