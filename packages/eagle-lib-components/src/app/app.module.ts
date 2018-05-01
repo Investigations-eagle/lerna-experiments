b35cc5da073ffa7c6d19be6699a8f6af4da0decb
Serhii Krechko <serhii.krechko@eleks.com>
2018-05-01 13:11:24 +0300

    (lib-components) Add base component

--- packages/eagle-lib-components/src/app/app.module.ts
+++ packages/eagle-lib-components/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EagleControlModule, LoginModule } from 'eagle-lib-components';
+import { BaseComponent } from './base/base.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([{
+      path: '', component: BaseComponent
+    }]),
     EagleControlModule,
     LoginModule,
   ],
